dfdd43858392459785173a880d438ce351bd4bd7
Hikaru Wada <watyaa@gmail.com>
2022-07-17 15:40:23 +0900

    Remove luxon

--- lib/date.ts
+++
@@ -1,5 +0,0 @@
-import { DateTime } from 'luxon'
-
-export const formatDate = (dateStr: string, format = DateTime.DATE_SHORT) => {
-  return DateTime.fromISO(dateStr).setZone('Asia/Taipei').toLocaleString(format)
-}
